e2d3fb2374fc9d2bca16df490ec632e2c0c26673
D-u-s-k <36265654+D-u-s-k@users.noreply.github.com>
2018-04-19 09:14:54 +0100

    Create sh_salesperson.lua

---
+++ schema/factions/sh_salesperson.lua
@@ -0,0 +1,8 @@
+FACTION.name = "fSalespersonName"
+FACTION.desc = "fSalespersonDesc"
+FACTION.color = Color(255, 0, 0)
+FACTION.isDefault = false
+--Add refugee model here
+FACTION.pay = 10
+
+FACTION_SP = FACTION.index
