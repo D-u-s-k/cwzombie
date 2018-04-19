ba8c7f049d8178d0059255a0a953f28f8f80dac7
D-u-s-k <36265654+D-u-s-k@users.noreply.github.com>
2018-04-19 11:45:57 +0100

    Create sh_english.lua

---
+++ schema/languages/sh_english.lua
@@ -0,0 +1,9 @@
+LANGUAGE = {
+	fCitizenName = "Citizen",
+	fCitizenDesc = "A regular citizen within the streets.",
+	fSalespersonName = "Salesperson",
+  fSalespersonDesc" = "A salesperson who you can buy things from.",
+
+	objectives = "Objectives",
+	prioritySet = "You have changed the priority status of %s.",
+}
